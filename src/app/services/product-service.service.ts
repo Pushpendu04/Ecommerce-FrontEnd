4ec49dcda6618ef9a91a488663bd6e588961aadb
Pushpendu Ghosh <pushpendu04@gmail.com>
2021-03-12 15:50:39 +0530

    initial commmit

---
+++ src/app/services/product-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { Product } from '../common/product';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl='http://localhost:8080/products'
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProductList():Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.baseUrl)
+  }
+}
